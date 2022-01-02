8c3126cc6a3da6b4f52b89fbad5816e61c4828f2
Ashad <mdashad7779@gmail.com>
2022-01-02 17:05:36 +0530

    everything kind of working fine but one time page disappears only after double click on next button maybe some glitch with temp variable

--- scripts/savedData.js
+++ scripts/savedData.js
@@ -1,10 +1,30 @@
+// DECLARE & INITIALIZE
 const topicBtn = document.querySelectorAll(".intBtn");
 const nextBtn = document.querySelector(".nextBtn");
 const storedTopics = [];
+const spModal = document.querySelector('.startupPage');
+const modal = document.querySelector('.modal');
+const mdText = document.querySelector('.modalText');
 const apiKey = "0b6ee3e8b2ffd5633d2402696d57cc66";
 let userCountry = "";
 let userCity = "";
+let lat;
+let lon;
+let tempCC ;
+let tempLL ;
 
+// DOM LOADED
+window.addEventListener('DOMContentLoaded',()=>{
+  getLoc();
+  setTimeout(()=>{
+    if(!lat && !lon){
+      toggleModal(3,removeModal)
+    }
+  },3000)
+})
+
+
+//  EVENT LISTENERS
 topicBtn.forEach((element) => {
   element.addEventListener("click", () => {
     storedTopics.push(element.id);
@@ -16,29 +36,102 @@ nextBtn.addEventListener("click", () => {
   const City = document.getElementById("cityInput").value;
   userCity = City;
   userCountry = Country;
-  let resp = checkCity();
+  console.log(tempCC);
+  checkApiCC();
+  console.log(tempCC);
+  if(lat && lon){
+    checkApiLL();
+  }
   if (storedTopics.length < 3) {
-    alert("Minimum 3 Topics");
+    toggleModal(1,removeModal);
+  }else{
+    setLocalStorage('pref',storedTopics);
+    
+    if(tempLL === true ){
+      console.log(`move to news`);
+      spModal.classList.add('sp-vis');
+      setLocalStorage('dataAV',true);
+    }else if(tempCC === true ){
+      console.log(`move to news`);
+      spModal.classList.add('sp-vis');
+      setLocalStorage('dataAV',true);
+    }
   }
-  if(resp === 'error'){
-    alert('wrong city / country');
 
-  }
 });
 
-const checkCity = () => {
-  let xhr = new XMLHttpRequest();
-  xhr.onload = () => {
-    if (xhr.status = 200) {
-        console.log(xhr.responseText);
-      return 'found'
-    }else{
-        return 'error'
-    }
-  };
-  xhr.open(
-    "GET",
-    `api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${apiKey}`,false
-  );
-  xhr.send();
-};
+
+// FUNCTIONS
+
+// check api with Country / City
+
+const checkApiCC = async ()=>{
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${apiKey}`);
+  const data = await response.json();
+  if(data.message){
+    console.log(data.message);
+    console.log(`CC failed`);
+    tempCC = false;
+  }else{
+    tempCC = true;
+    console.log(`cc working`,data,storedTopics);
+    setLocalStorage(`userCity`,userCity);
+    setLocalStorage(`userCountry`,userCountry);
+  }
+}
+
+// check if api working with LL
+const checkApiLL = async () =>{
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+  const data = await response.json();
+  if(data){
+    console.log(`LL working`);
+    tempLL = true;
+    setLocalStorage(`userLatitude`,lat);
+    setLocalStorage(`userLongitude`,lon);
+  }else{
+    console.log(`LL Failed`);
+    tempLL = false;
+  }
+}
+
+
+// get user loc
+function getLoc(){
+    navigator.geolocation.getCurrentPosition(function(pos){
+      console.log(pos);
+      lat = pos.coords.latitude;
+      lon = pos.coords.longitude;
+      })
+}
+
+// show / hide popup
+function toggleModal(n,callback){
+  if(n==1){
+    modal.classList.remove('md-vis');
+    mdText.textContent = 'Please select minimum 3 topics ';
+    setTimeout(callback,1500);
+  }else if(n==2){
+    modal.classList.remove('md-vis');
+    mdText.textContent = 'Wrong City / Country '
+    setTimeout(callback,1500);
+  }else if(n==3){
+    modal.classList.remove('md-vis');
+    mdText.textContent = 'Cannot get access to location'
+    setTimeout(callback,1500);
+  }else if(n==4){
+    modal.classList.remove('md-vis');
+    mdText.textContent = 'Cannot get Weather report'
+    setTimeout(callback,1500);
+  }
+}
+
+function removeModal(){
+  modal.classList.add('md-vis');
+}
+
+
+//set items to local storage
+function setLocalStorage(key,value){
+  localStorage.setItem(key,value);
+}
